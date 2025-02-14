ef83b2e8f45e6b226a91d24cde90bb7e5abff151
Frida.Nord <Frida.Nord@medieinstitutet.se>
2025-02-14 13:01:04 +0100

    Rättat nummber 2, göra om konstruktor?

--- main.ts
+++ main.ts
@@ -12,5 +12,22 @@
     return total;
   }
 
-
-
+  /*
+    2. I detta exempel har vi fokuserat på if-statements. Se om du kan göra exemplet bättre!
+    */
+  
+    class Student {
+        constructor(
+          public name: string,
+          public handedInOnTime: boolean,
+          public passed: boolean
+        ) {}
+      }
+      
+      function getStudentStatus(student: Student): string {
+        student.passed = student.name === "Sebastian" &&
+        student.handedInOnTime;
+        return student.passed? "VG":"IG";
+      }
+    
+    
