14769ba66794d3292895243fd9e99d9ad8a74394
Frida.Nord <Frida.Nord@medieinstitutet.se>
2025-02-14 12:52:29 +0100

    rättat 1.

---
+++ main.ts
@@ -0,0 +1,16 @@
+/*
+  1. Se om du kan hitta två stycken code smells i följande funktion och rätta till dem.
+  Funktionen tar emot en lista med längshoppslängder och syftet med funktionen är att summera
+  dessa hopplängder.
+  */
+
+  function getLength(jumpings: number[]): number {
+    let total = 0;
+    for (const jump of jumpings) {
+        total += jump;
+    }
+    return total;
+  }
+
+
+
