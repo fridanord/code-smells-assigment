4348d5b356cf42b73c7ac9497e170cb53cc2cb81
Frida.Nord <Frida.Nord@medieinstitutet.se>
2025-02-14 18:23:13 +0100

    Rättat, men kommer  gå tillbaka för att se om det kan göras bättre

--- main.ts
+++ main.ts
@@ -56,3 +56,61 @@
       
         return totalTemp / 7;
       }
+
+  /*
+    4. Följande funktion kommer att presentera ett objekt i dom:en. 
+    Se om du kan göra det bättre. Inte bara presentationen räknas, även strukturer.
+    */
+  
+    function showProduct(
+        { name, price, amount, description, image}: {name: string, price: number, amount: number, description: string, image: string },
+        parent: HTMLElement
+    ) {
+        const container = document.createElement("div");
+
+        container.innerHTML = `
+        <h4>${name}</h4>
+        <img src="${image}" alt=${name}" />
+        <strong>${price}</strong>
+        `;
+    parent.appendChild(container);
+    }
+
+
+/*
+    5. Följande funktion kommer presentera studenter. Men det finns ett antal saker som 
+    går att göra betydligt bättre. Gör om så många som du kan hitta!
+    */
+    
+    interface Student { //Interface för att kunna återanvända kod
+        name: string;
+        handedInOnTime: boolean;
+    }
+
+    function createCheckbox(checked: boolean): HTMLInputElement {
+        const checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        checkbox.checked = checked; //Finns det bättre sätt för att se om den är true eller false? 
+        return checkbox;
+    }
+
+    function appendStudentToList(student: Student, listId: string) {
+        const container = document.createElement("div");
+        const checkbox = createCheckbox(student.handedInOnTime);
+
+        container.appendChild(checkbox);
+        const list = document.querySelector<HTMLUListElement>(`ul#${listId}`); // Göra det bättre? Kolla tidigare kod
+        list?.appendChild(container);
+
+    }
+
+    function presentStudents(students: Student[]) {
+        const passedStudents = students.filter(student => student.handedInOnTime);
+        const failedStudents = students.filter(student => !student.handedInOnTime);
+
+        passedStudents.forEach(student => appendStudentToList(student, "passedstudents"));
+        failedStudents.forEach(student => appendStudentToList(student, "failedstudents"));
+    }
+
+
+
